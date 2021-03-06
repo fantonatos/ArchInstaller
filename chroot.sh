b687559cf00f9186166ee15defb942c9292b7328
Fotis Antonatos <fotis.antonatos7@gmail.com>
2021-03-05 21:17:00 -0500

    Added chroot script

---
+++ chroot.sh
@@ -0,0 +1,56 @@
+# This script runs inside the target machine
+
+# Configure the system time and localization
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+touch /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# Setup hostname and user accounts
+echo ""
+echo "Enter the hostname: "
+read HOSTNAME
+
+touch /etc/hostname
+echo "${HOSTNAME}" >> /etc/hostname
+
+touch /etc/hosts
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	${HOSTNAME}.localdomain	${HOSTNAME}"
+
+echo "Set root password: "
+passwd
+
+echo "Adding new user."
+echo "Enter username: "
+read USERNAME
+useradd -m ${USERNAME}
+passwd USERNAME
+usermod -aG wheel,audio,video,optical,storage ${USERNAME}
+
+# Install the bootloader
+echo ""
+echo "Installing Grub Bootloader..."
+echo ""
+
+pacman -S grub efibootmgr dosfstools os-prober mtools
+
+mkdir /boot/EFI
+mount /dev/sda1 /boot/EFI
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+
+# Install extra software
+echo ""
+echo "Installing more software..."
+echo ""
+
+pacman -S  vim man-db man-pages  networkmanager git
+
+# Exit the chroot
+exit
--- install-essentials.sh
+++ install-essentials.sh
@@ -1,3 +1,4 @@
+echo ""
 lsblk -f
 echo ""
 echo "Select the Operating System Partition: "
@@ -10,59 +11,14 @@ swapon ${SWAPDISK}
 
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
 
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-touch /etc/locale.conf
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+# Copy chroot.sh to the new system and run the script
+cp chroot.sh /mnt/chroot.sh
+arch-chroot /mnt ./chroot.sh
 
-echo ""
-echo "Enter the hostname: "
-read HOSTNAME
-
-touch /etc/hostname
-echo "${HOSTNAME}" >> /etc/hostname
-
-touch /etc/hosts
-echo "127.0.0.1	localhost" >> /etc/hosts
-echo "::1		localhost" >> /etc/hosts
-echo "127.0.1.1	${HOSTNAME}.localdomain	${HOSTNAME}"
-
-echo "Set root password: "
-passwd
-
-echo "Adding new user."
-echo "Enter username: "
-read USERNAME
-useradd -m ${USERNAME}
-passwd USERNAME
-usermod -aG wheel,audio,video,optical,storage ${USERNAME}
-
-echo ""
-echo "Installing Grub Bootloader..."
-echo ""
-
-pacman -S grub efibootmgr dosfstools os-prober mtools
-
-mkdir /boot/EFI
-mount /dev/sda1 /boot/EFI
-grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-
-echo ""
-echo "Installing more software..."
-echo ""
-
-pacman -S  vim man-db man-pages  networkmanager git
-
-exit
+# We are back out of the chroot.
+rm /mnt/chroot.sh
 umount -l /mnt
-
 echo ""
 echo "Installation Complete."
-echo "Remove installation media and reboot."
+echo "Please remove installation media and reboot."
