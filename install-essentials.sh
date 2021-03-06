7946a7c6a5ac2075b71748abde53e8fbb6043c41
Fotis Antonatos <fotis.antonatos7@gmail.com>
2021-03-05 21:05:30 -0500

    Fixed issues

--- install-essentials.sh
+++ install-essentials.sh
@@ -3,20 +3,19 @@ echo ""
 echo "Select the Operating System Partition: "
 read DISK
 echo "Select the Swap Partition: "
+read SWAPDISK
 
 mount ${DISK} /mnt
-swapon ${DISK}
+swapon ${SWAPDISK}
 
-pacstrap /mnt base linux linux-firmware vim man-db man-pages grub efibootmgr dosfstools os-prober mtools networkmanager git
+pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
 
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
-
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-
 touch /etc/locale.conf
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
@@ -43,8 +42,10 @@ passwd USERNAME
 usermod -aG wheel,audio,video,optical,storage ${USERNAME}
 
 echo ""
-echo "Installing Grub Bootloader"
+echo "Installing Grub Bootloader..."
 echo ""
+
+pacman -S grub efibootmgr dosfstools os-prober mtools
 
 mkdir /boot/EFI
 mount /dev/sda1 /boot/EFI
@@ -52,6 +53,13 @@ grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
+
+echo ""
+echo "Installing more software..."
+echo ""
+
+pacman -S  vim man-db man-pages  networkmanager git
+
 exit
 umount -l /mnt
 
