02d210ba9115e7b30469dcad2c271edb8dfc350d
fantonatos <fotis.antonatos7@gmail.com>
2021-12-23 00:01:22 -0500

    GNOME desktop, README, LICENSE
    installer.sh: Reads partitions from command-line arguments.
    desktop.sh: Removed openbox, xinit, SegeoUI fonts, switched to GNOME desktop.
    README.md: Improved instructions, added system details and known issue.
    LICENSE: Added the ISC License.

--- installer.sh
+++ installer.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Script to automate the ArchLinux installation process.
+# Usage: installer.sh <boot> <swap> <root>
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: installer.sh <boot> <swap> <root>"
+    echo "    boot, swap, root:"
+    echo "      Path to device (/dev/device_name)"
+    exit
+fi
 
 if [[ ! -d /sys/firmware/efi/efivars ]]
 then
@@ -6,18 +15,20 @@ then
     exit
 fi
 
+UEFI=$1
+SWAP=$2
+ROOT=$3
+echo "boot partition: ${UEFI:?}"
+echo "swap partition: ${SWAP:?}"
+echo "root partition: ${ROOT:?}"
+
+read -rp "Are these correct? "
 read -rp "Have you checked your Internet connection? "
-read -rp "Have you formatted your disks? If not, do so now. "
+read -rp "Have you partitioned and formatted your disks? "
+read -rp "Proceed with the installation? Press CTRL-C to cancel. "
 
 echo "Enabling network time"
 timedatectl set-ntp true
-
-echo "---------------------------------"
-lsblk -f
-echo "---------------------------------"
-read -rp "Enter the UEFI Boot partition: " UEFI
-read -rp "Enter the operating system partition: " ROOT
-read -rp "Enter the swap partition: " SWAP
 
 mount "${ROOT}" /mnt
 mkdir -p /mnt/boot && mount "${UEFI}" /mnt/boot
@@ -26,7 +37,7 @@ swapon "${SWAP}"
 pacstrap /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 
-# Copy chroot.sh into the new system and run it
+# 1st chroot: Copy chroot.sh into the new system and run it
 echo "-------------------------------------"
 echo "chrooting into the new system"
 echo "-------------------------------------"
@@ -38,19 +49,20 @@ arch-chroot /mnt ./chroot.sh "${UEFI}" "${ROOT}"
 # We are back out of the chroot
 rm /mnt/chroot.sh
 rm -rf /mnt/files/
+echo ""
 echo "Minimal installation completed."
-echo "Next step will install xorg and a desktop environment."
+echo "Next step will install xorg and GNOME desktop."
 read -rp "If you wish to make further changes, or do not want a graphical environment, press CTRL-C to exit and leave partitions mounted. "
 
-# Do another chroot with desktop.sh
+# 2nd chroot: run desktop.sh
 cp desktop.sh /mnt/desktop.sh
 arch-chroot /mnt ./desktop.sh "$(ls /home)"
-
-# Back out of 2nd chroot
 rm /mnt/desktop.sh
-read -rp "Full installation completed. If you need to make further changes, press CTRL-C to exit and leave partitions mounted. "
 
+
+read -rp "Full installation completed. If you need to make further changes, press CTRL-C to exit and leave partitions mounted. "
 umount -l /mnt
 echo ""
 echo "Partitions were unmounted."
 echo "You can remove installation media and reboot into the new system."
+
